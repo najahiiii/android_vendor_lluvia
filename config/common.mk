6bb010e47e487e6ecdc18b588e96adf810f205e6
ean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-01 10:02:52 +0200

    Don't compile SystemUITests
    Change-Id: Ib5114a12ecf2c6c3918a7d99867913946449a97e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/lluvia/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 
 
+# Don't compile SystemUITests
+EXCLUDE_SYSTEMUI_TESTS := true
+
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/lluvia/overlay/common
 
