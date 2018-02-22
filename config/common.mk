17c12c6656b9e378577d097ca4ba3c64801f76ec
Paul Keith <javelinanddart@gmail.com>
2018-02-22 14:06:54 +0100

    lluvia: Unconditionally ship exFAT tools
     * We no longer need to build the mount.exfat tool because
       only kernel implementations will be supported from now on.
    
    Change-Id: Ic7e1354e6ead6a96a9d6021de8dac4cfdabcbbce
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -68,6 +68,11 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
+# exFAT tools
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    mkfs.exfat
+
 # Media
 PRODUCT_GENERIC_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
