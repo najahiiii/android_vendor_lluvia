e138be97cfe8d3471458b68679d03cc4ae137bff
camcory <camcory1@gmail.com>
2018-04-06 09:46:38 -0400

    We dont do engineering builds
    Change-Id: I40869a898c4873c39e07cd00d138d310d05e8291

--- config/common.mk
+++ config/common.mk
@@ -45,11 +45,6 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-PRODUCT_GENERIC_PROPERTIES += ro.adb.secure=1
-endif
-
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
