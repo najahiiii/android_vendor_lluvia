5db256eb743a72512b62fdb6a517fe34dffec5c0
leodiram <sriramscience7@gmail.com>
2018-08-03 16:25:06 +0530

    Add init and mkshrc
    Change-Id: Idf0ea2b206b777a429b990bdb7bd1268cd44328d

--- config/common.mk
+++ config/common.mk
@@ -22,5 +22,13 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/lluvia/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/lluvia/prebuilt/common/etc/init.local.rc:root/init.aosip.rc
+
+# Don't export PS1 in /system/etc/mkshrc.
+PRODUCT_COPY_FILES += \
+    vendor/lluvia/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
 # Packages
 include vendor/lluvia/config/packages.mk
