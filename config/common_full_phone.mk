AOSiP/platform_vendor_aosip

Selective based spn/operatorCheck
Change-Id: I3ae981fdb4183a015435e07576306f3580ee8834

config/common_full_phone.mk
@@ -1,0 +1,10 @@
+# Inherit common stuff
+$(call inherit-product, vendor/aosip/config/common.mk)
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk

najahiiii/android_vendor_lluvia

config/common_full_phone.mk
@@ -1,0 +1,10 @@
+# Inherit common stuff
+$(call inherit-product, vendor/lluvia/config/common.mk)
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/lluvia/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk