CarbonROM/android_vendor_carbon

sdclang: Gaurd usage
Change-Id: Ie3e1c8130a7b10e2a721c66fe247a4cf6e8373a8
Signed-off-by: Joe Maples <joe@frap129.org>

sdclang/sdclang.mk
@@ -17,6 +17,6 @@ SDCLANG_FORCED ?= false
 
 SDCLANG_PATH := vendor/qcom/sdclang-3.8/linux-x86/bin
 
-SDCLANG_LTO_DEFS := device/qcom/common/sdclang/sdllvm-lto-defs.mk
+SDCLANG_LTO_DEFS := vendor/carbon/sdclang/sdllvm-lto-defs.mk
 
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize-loops

najahiiii/android_vendor_lluvia

Signed-off-by: leodiram <sriramscience7@gmail.com>

sdclang/sdclang.mk
@@ -17,6 +17,6 @@ SDCLANG_FORCED ?= false
 
 SDCLANG_PATH := vendor/qcom/sdclang-3.8/linux-x86/bin
 
-SDCLANG_LTO_DEFS := device/qcom/common/sdclang/sdllvm-lto-defs.mk
+SDCLANG_LTO_DEFS := vendor/lluvia/sdclang/sdllvm-lto-defs.mk
 
 SDCLANG_COMMON_FLAGS := -O3 -fvectorize-loops