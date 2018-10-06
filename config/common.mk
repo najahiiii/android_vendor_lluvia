1cebb6ee27c26147ebf6bfed0d778f9b3a1aef19
Srijith <srijithdamerla2001@gmail.com>
2018-10-06 09:07:52 +0530

    vendor: Add our bootanimation
    Signed-off-by: Srijith <srijithdamerla2001@gmail.com>
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -102,11 +102,11 @@ endif
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation_720.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation_1080.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation_1440.zip:system/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/lluvia/media/bootanimation.zip:system/media/bootanimation.zip
 else
     $(error "Bootanimation: TARGET_BOOT_ANIMATION_RES is undefined")
 endif
