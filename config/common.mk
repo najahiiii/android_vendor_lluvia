eldainosor/vendor_pixys

Create OFFICAL buildtype

config/common.mk
@@ -4,6 +4,14 @@ PIXYS_VERSION_NUMBER := 1.0
 
 ifndef PIXYS_BUILDTYPE
 PIXYS_BUILDTYPE := UNOFFICIAL
+
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.pixys.buildtype=unofficial
+endif
+
+ifeq ($(PIXYS_BUILDTYPE), OFFICIAL)
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.pixys.buildtype=official
 endif
 
 PIXYS_VERSION := PixysOS-$(PIXYS_VERSION_NUMBER)-$(shell date -u +%Y%m%d)-$(PIXYS_BUILDTYPE)-$(PIXYS_BUILD)

najahiiii/android_vendor_lluvia

Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

config/common.mk
@@ -2,8 +2,17 @@ PRODUCT_BRAND ?= LLuvia
 
 LLUVIA_VERSION_NUMBER := 3.0
 
-ifndef LLUVIA_BUILDTYPE
-LLUVIA_BUILDTYPE := ManMade
+ifndef LLUVIA_BUILD_TYPE
+LLUVIA_BUILD_TYPE := ManMade
+
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.lluvia.buildtype=ManMade
+endif
+
+ifeq ($(LLUVIA_BUILD_TYPE), OFFICIAL)
+LLUVIA_BUILD_TYPE := NatureMade
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.lluvia.buildtype=NatureMade
 endif
 
 LLUVIA_VERSION := LLuviaOS-$(LLUVIA_VERSION_NUMBER)-$(shell date -u +%Y%m%d)-$(LLUVIA_BUILDTYPE)-$(LLUVIA_BUILD)