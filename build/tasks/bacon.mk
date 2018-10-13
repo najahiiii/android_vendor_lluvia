eldainosor/vendor_pixys

Fix output derp

build/tasks/bacon.mk
@@ -20,7 +20,7 @@ ifneq ($(BUILD_WITH_COLORS),0)
     include $(TOP_DIR)vendor/pixys/build/core/colors.mk
 endif
 
-PIXYS_TARGET_PACKAGE := $(PRODUCT_OUT)/PixysOS-$(PIXYS_VERSION).zip
+PIXYS_TARGET_PACKAGE := $(PRODUCT_OUT)/$(PIXYS_VERSION).zip
 
 .PHONY: bacon pixys
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)

najahiiii/android_vendor_lluvia

Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

build/tasks/bacon.mk
@@ -20,7 +20,7 @@ ifneq ($(BUILD_WITH_COLORS),0)
     include $(TOP_DIR)vendor/lluvia/build/core/colors.mk
 endif
 
-LLUVIA_TARGET_PACKAGE := $(PRODUCT_OUT)/LLuviaOS-$(PIXYS_VERSION).zip
+LLUVIA_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LLUVIA_VERSION).zip
 
 .PHONY: bacon rain
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)