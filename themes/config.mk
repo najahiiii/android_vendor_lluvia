1c7687b1ecb6cfe2b5c4e39836d6304a8292d536
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2018-10-13 04:07:02 +0000

    lluvia: Lets Build out Theme
    Special Thanks PixelExperiance and Other team for their great work.
    
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

---
+++ themes/config.mk
@@ -0,0 +1,35 @@
+# Primary Themes
+PRODUCT_PACKAGES += \
+    DarkThemeAndroidOverlay \
+    DarkThemeSettingsOverlay \
+    BlackThemeAndroidOverlay \
+    BlackThemeSettingsOverlay
+
+# Accents colors
+PRODUCT_PACKAGES += \
+    AmberAccent \
+    BlackAccent \
+    BlueAccent \
+    BlueGreyAccent \
+    BrownAccent \
+    CyanAccent \
+    DeepOrangeAccent \
+    DeepPurpleAccent \
+    GreenAccent \
+    GreyAccent \
+    IndigoAccent \
+    LightBlueAccent \
+    LightGreenAccent \
+    LimeAccent \
+    OrangeAccent \
+    PinkAccent \
+    PurpleAccent \
+    RedAccent \
+    TealAccent \
+    YellowAccent \
+    WhiteAccent
+
+# Fonts
+#PRODUCT_PACKAGES += \
+#    CustomFonts
+
