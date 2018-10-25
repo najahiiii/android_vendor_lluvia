35f7bc1fc9c723227761ce85af7884a359f37ebe
Subinsmani <subins.mani@gmail.com>
2018-10-26 00:17:07 +0530

    [vendor] Add LLuvia Display Version to build/core/main_version
    Change-Id: I285995e67baa0806cd5e62544d4df0cf951126c7
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,5 +1,6 @@
 # LLuviaOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.lluvia.version_number=$(LLUVIA_VERSION_NUMBER) \
-    ro.lluvia.releasetype=$(LLUVIA_BUILDTYPE) \
-    ro.lluvia.version=$(LLUVIA_VERSION)
+    ro.lluvia.releasetype=$(LLUVIA_BUILD_TYPE) \
+    ro.lluvia.version=$(LLUVIA_VERSION) \
+    ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION)
