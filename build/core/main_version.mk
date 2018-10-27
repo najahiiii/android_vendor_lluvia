f84712fb17b3972d20f62d061f4ce6fc934a4d76
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2018-10-27 12:14:38 +0530

    main_version: Add modversion to be built

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -3,4 +3,5 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.lluvia.version_number=$(LLUVIA_VERSION_NUMBER) \
     ro.lluvia.releasetype=$(LLUVIA_BUILD_TYPE) \
     ro.lluvia.version=$(LLUVIA_VERSION) \
-    ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION)
+    ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION) \
+    ro.modversion=$(LLUVIA_MOD_VERSION)
